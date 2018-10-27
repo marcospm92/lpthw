ffa3496f2cb0ff149274256de6f3b03cb1987192
marcospm92 <marcospm92@gmail.com>
2018-10-27 23:46:23 +0200

    Exercises 2, 3, and second version of ex3 with floating numbers

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:")
+# first 30/6 (5) then +25 (30)
+print("Hens", 25 + 30 / 6)
+# first 25*3 (75), then 75%4 (3), last 100-3 (97)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+# first 4%2 (0), then 1/4 (0.25), last 3+2+1-5+0-0.25+6 (6.75)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7 ?")
+# Is 5<-7? No
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2 ?", 3 + 2)
+print("What is 5 - 7 ?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is is greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex3_1.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:")
+# first 30/6 (5) then +25 (30)
+print("Hens", 25.0 + 30.0 / 6.0)
+# first 25*3 (75), then 75%4 (3), last 100-3 (97)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Now I will count the eggs:")
+# first 4%2 (0), then 1/4 (0.25), last 3+2+1-5+0-0.25+6 (6.75)
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3 + 2 < 5 - 7 ?")
+# Is 5<-7? No
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2 ?", 3.0 + 2.0)
+print("What is 5 - 7 ?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > -2.0)
+print("Is is greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
